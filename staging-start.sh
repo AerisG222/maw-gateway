ae62efaf1a49ec152a078900bf9b4c65a265a842
Mike Morano <mmorano@mikeandwan.us>
2025-10-18 22:20:28 -0400

    update scripts

--- staging-start.sh
+++ staging-start.sh
@@ -2,8 +2,8 @@
 
 podman run --rm \
     --name staging-maw-gateway \
-    --publish "8000:80" \
-    --publish "8443:443" \
+    --publish "8000:8000" \
+    --publish "8443:8443" \
     --volume "/home/mmorano/maw-gateway/staging/certs/:/certs:ro,Z" \
     --volume "/home/mmorano/maw-gateway/staging/certbot-notused-in-staging:/certbot:ro,z" \
     --volume "/home/mmorano/maw-gateway/staging/certbot-challenge-notused-in-staging:/certbot-challenge:ro,z" \
